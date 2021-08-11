a712f467d80f8c385576e1b46235aeddcda69349
xpedroleonardo <aispedroleonardo@gmail.com>
2021-08-11 18:51:47 -0300

    update profile method

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,22 +1,62 @@
-import Header from "../../components/Header";
+import { useState } from "react";
+import { FormEvent } from "react";
+import { useHistory } from "react-router-dom";
+import { Main, Group } from "./styles";
+import { useEffect } from "react";
 
+import Header from "../../components/Header";
 import avatarImg from "../../assets/images/avatar.svg";
 import fileImg from "../../assets/images/file.svg";
+import api from "../../services/api";
 
-import { Main, Group } from "./styles";
+type ProfileProps = {
+  name: string;
+  avatar: string;
+};
 
 function Profile() {
+  const { push } = useHistory();
+  const [image, setImage] = useState("");
+  const [user, setUser] = useState<ProfileProps>();
+
+  const url = "http://localhost:3333/image/";
+  const ImageProfile = user?.avatar ? `${url + user.avatar}` : avatarImg;
+
+  useEffect(() => {
+    api.get("/user").then((res) => {
+      const { name, avatar } = res.data;
+      setUser({ name, avatar });
+    });
+  }, []);
+
   function imagePreview(event: any) {
-    const name = event.target.files[0].name;
+    const name = event.target.files[0]?.name || "";
     const img = document.querySelector(".avatar");
     const labelImg = document.querySelector("#image span");
+    setImage(event.target.files[0]);
 
-    console.log(event);
-    if (img) {
+    if (img && name !== "") {
       let preview = URL.createObjectURL(event.target.files[0]);
+
       img.setAttribute("src", preview);
       if (labelImg) labelImg.innerHTML = name;
     }
+  }
+
+  async function formSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name } = event.target as typeof event.target & {
+      name: { value: string };
+    };
+
+    const data = new FormData();
+    data.append("name", name.value);
+    const imageForm = image ? image : String(user?.avatar);
+
+    data.append("avatar", imageForm);
+
+    await api.put("/update/user", data).then(() => push("/"));
   }
 
   return (
@@ -24,14 +64,19 @@ function Profile() {
       <Header title="Perfil" />
       <Main>
         <div className="container animate-up">
-          <form action="" autoComplete="off" encType="multipart/form-data">
+          <form
+            onSubmit={formSubmit}
+            autoComplete="off"
+            encType="multipart/form-data"
+          >
             <section>
               <img
-                src={avatarImg}
+                src={ImageProfile}
                 className="avatar"
                 draggable="false"
                 alt="Foto de Perfil"
               />
+
               <Group>
                 <label htmlFor="name">Nome</label>
                 <input
@@ -40,20 +85,20 @@ function Profile() {
                   id="name"
                   required
                   placeholder="Digite seu nome..."
+                  defaultValue={user?.name ? user.name : ""}
                 />
               </Group>
               <Group>
                 <label htmlFor="image">Imagem</label>
                 <label htmlFor="imageInput" id="image">
                   <img src={fileImg} className="icon" alt="" />
-                  <span> Selecione uma imagem </span>
+                  <span>Selecione uma imagem</span>
                 </label>
                 <input
                   onChange={imagePreview}
                   type="file"
-                  name="image"
+                  name="avatar"
                   id="imageInput"
-                  required
                 />
               </Group>
               <Group>
